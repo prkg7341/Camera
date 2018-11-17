1d6ea9ac8330001cbc595cf8a29817ea1b99ce4e
prkg7341 <40709295+prkg7341@users.noreply.github.com>
2018-11-17 19:33:54 +0900

    복구

---
+++ app/src/main/java/com/jaewoo/camera/MainActivity.java
@@ -0,0 +1,18 @@
+package com.jaewoo.camera;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (null == savedInstanceState) {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.container, Camera.newInstance())
+                    .commit();
+        }
+    }
+}
