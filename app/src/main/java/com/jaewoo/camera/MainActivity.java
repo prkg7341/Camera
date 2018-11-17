05c8b9280eb341f823dfde2d42be6fffc092fd63
prkg7341 <40709295+prkg7341@users.noreply.github.com>
2018-11-17 20:49:04 +0900

    상태바 없애기

--- app/src/main/java/com/jaewoo/camera/MainActivity.java
+++ app/src/main/java/com/jaewoo/camera/MainActivity.java
@@ -2,12 +2,22 @@ package com.jaewoo.camera;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // 상태바를 안보이도록 합니다.
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // 화면 켜진 상태를 유지합니다.
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
         setContentView(R.layout.activity_main);
         if (null == savedInstanceState) {
             getFragmentManager().beginTransaction()
