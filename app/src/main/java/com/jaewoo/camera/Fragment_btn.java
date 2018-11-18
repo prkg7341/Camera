98ecd6dca966b078c0116950ecb13f8d2564bf8d
prkg7341 <40709295+prkg7341@users.noreply.github.com>
2018-11-18 21:09:23 +0900

    갤러리 호출

--- app/src/main/java/com/jaewoo/camera/Fragment_btn.java
+++ app/src/main/java/com/jaewoo/camera/Fragment_btn.java
@@ -1,7 +1,8 @@
 package com.jaewoo.camera;
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,7 +28,11 @@ public class Fragment_btn extends Fragment implements View.OnClickListener{
                 break;
 
             case R.id.btn_gallery:
+                Intent intent = new Intent(Intent.ACTION_PICK);
 
+                intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
+
+                startActivity(intent);
 
                 break;
             case R.id.btn_frame:
