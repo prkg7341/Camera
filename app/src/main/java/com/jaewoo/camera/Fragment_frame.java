abbe514c8fede0d8c4f183cc4f4aa4194c84b3c3
cksungsung <ck.sung@ds.seoultech.ac.kr>
2018-11-19 15:01:34 +0900

    버튼9의 클릭시 리액션 추가

--- app/src/main/java/com/jaewoo/camera/Fragment_frame.java
+++ app/src/main/java/com/jaewoo/camera/Fragment_frame.java
@@ -36,27 +36,30 @@ public class Fragment_frame extends Fragment implements View.OnClickListener {
         try {
             switch (v.getId()) {
                 case R.id.button1:
-                    MainActivity.camera.setImage(R.drawable.person2);
+                    MainActivity.camera.setImage(R.drawable.person1);
                     break;
                 case R.id.button2:
-                    MainActivity.camera.setImage(R.drawable.person3);
+                    MainActivity.camera.setImage(R.drawable.person2);
                     break;
                 case R.id.button3:
-                    MainActivity.camera.setImage(R.drawable.person4);
+                    MainActivity.camera.setImage(R.drawable.person3);
                     break;
                 case R.id.button4:
-                    MainActivity.camera.setImage(R.drawable.person5);
+                    MainActivity.camera.setImage(R.drawable.person4);
                     break;
                 case R.id.button5:
-                    MainActivity.camera.setImage(R.drawable.person6);
+                    MainActivity.camera.setImage(R.drawable.person5);
                     break;
                 case R.id.button6:
-                    MainActivity.camera.setImage(R.drawable.person7);
+                    MainActivity.camera.setImage(R.drawable.person6);
                     break;
                 case R.id.button7:
-                    MainActivity.camera.setImage(R.drawable.person8);
+                    MainActivity.camera.setImage(R.drawable.person7);
                     break;
                 case R.id.button8:
+                    MainActivity.camera.setImage(R.drawable.person8);
+                    break;
+                case R.id.button9:
                     MainActivity.camera.setImage(R.drawable.person9);
                     break;
             }
